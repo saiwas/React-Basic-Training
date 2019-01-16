4ab5397d0e8e2d0615bf2c05cf9c682825626934
Johnny <jzhang@xogrp.com>
2019-01-16 17:35:22 +0800

    React-Redux Demo - Part 3 - Update State Immutable way

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -31,17 +31,42 @@ class App extends Component {
         <div>Name: <span>{this.props.name}</span></div>
         <button onClick={this.props.onAgeUp}>Age Up</button>
         <button onClick={this.props.onAgeDown}>Age Down</button>
+
+        <hr />
+
+        <div>
+          The Change History:
+          <ul>
+            {
+              this.props.changeHistory.map((el) => (
+                <li key={el.key}
+                  className='historyItem'
+                  onClick={this.props.onDelItem.bind(this, el.key)}
+                >
+                  {el.age}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({age: state.age})
+const mapStateToProps = (state) => {
+  return {
+    age: state.age,
+    changeHistory: state.changeHistory
+  }
+}
 
 const mapDispatchToProps = (dispatch)  => {
   return {
-    onAgeUp: () => dispatch({type: 'AGE_UP'}),
-    onAgeDown: () => dispatch({type: 'AGE_DOWN'})
+    onAgeUp: () => dispatch({type: 'AGE_UP', value: 2}),
+    onAgeDown: () => dispatch({type: 'AGE_DOWN', value: 1}),
+    onDelItem: (key) => dispatch({type: 'DEL_ITEM', key: key})
   }
 }
 
--- react-redux-demo/src/store/reducer.js
+++ react-redux-demo/src/store/reducer.js
@@ -1,6 +1,7 @@
 const initialState = {
     name: 'Johnny',
-    age: 21
+    age: 21,
+    changeHistory: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -8,10 +9,23 @@ const reducer = (state = initialState, action) => {
 
     switch(action.type){
         case 'AGE_UP':
-            ++newState.age
+            newState.age += action.value;
+            newState.changeHistory = state.changeHistory.concat({
+                key: Math.random(),
+                name: newState.name,
+                age: newState.age
+            });
             break;
         case 'AGE_DOWN':
-            --newState.age
+            newState.age -= action.value;
+            newState.changeHistory = state.changeHistory.concat({
+                key: Math.random(),
+                name: newState.name,
+                age: newState.age
+            });
+            break;
+        case 'DEL_ITEM':
+            newState.changeHistory = state.changeHistory.filter(el => el.key !== action.key)
             break;
         default:
             break;
